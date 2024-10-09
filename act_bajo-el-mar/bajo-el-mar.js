f35ad68ec11ca9864deec0263220c5f36df1637a
Guiem Portàtil <guillembagurmoll@gmail.com>
2024-10-09 21:07:29 +0200

    añadir comentarios

--- act_bajo-el-mar/bajo-el-mar.js
+++ act_bajo-el-mar/bajo-el-mar.js
@@ -1,19 +1,24 @@
 const imagenSonda = "~~~><>~~~><>><>~~~><>~~~~~~><>~><>~";
 
+// Iniciamos el recuento en 0
 let recuento = 0;
+
 function contarPeces(cadena) {
-    const iconoPez = "><>";
+    const iconoPez = "><>"; // Los peces están identificados así
 
     const primerPezPos = cadena.indexOf(iconoPez);
 
+    // Si no se encuentra ningún pez, retornar 0
     if (primerPezPos < 0) {
         return 0;
     }
 
-    if (cadena.slice(primerPezPos, primerPezPos + iconoPez.length) === iconoPez) {
-        recuento ++;
-        return contarPeces(cadena.slice(primerPezPos + iconoPez.length));
-    }
+    // Si llegamos hasta aquí, significa que se ha encontrado otro pez
+    recuento++;
+    // Devolvemos la cadena pero recortando los caracteres que conforman el pez actual
+    return contarPeces(cadena.slice(primerPezPos + iconoPez.length));
+
+
 
 }
 
